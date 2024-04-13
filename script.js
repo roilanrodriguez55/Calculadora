e77b2e8dcb712950e3a2a37fe637e5275ee82dc7
Roilan Rodriguez Castillo <roilanrodriguez55@gmail.com>
2024-04-12 23:02:41 -0400

    Agregando ultimas correcciones

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Aqui va la nueva solucion que le voy a hacer en la logica
 const resultado = document.querySelector(".show-result")
 let num1 = "",
   num2 = "",
@@ -12,6 +11,8 @@ const reset = document.querySelector("#reset")
 const igual = document.querySelector("#igual")
 const punto = document.querySelector("#punto")
 const borrar = document.querySelector("#borrar")
+let calculos = []
+const resetBtn = document.querySelector("#reset-historial")
 
 // Cargar los botones con los numeros
 for (let i = 0; i < 10; i++) {
@@ -26,26 +27,26 @@ for (let i = 0; i < 4; i++) {
 numeros.forEach(n => {
   n.addEventListener("click", e => {
     resultado.textContent = ""
-    if (
-      (banderaNum === 0 && cantSymbols === 0) ||
-      (banderaNum === 0 && cantSymbols === 1)
-    ) {
+    if ((banderaNum === 0 && cantSymbols === 0) || (banderaNum === 0 && cantSymbols === 1)) {
       if (num1.length < 8) {
         num1 += e.target.textContent
         resultado.textContent += num1
+        console.log("simb-", cantSymbols);
+        console.log("bandera-", banderaNum);
+        return
       } else {
         resultado.textContent = num1
         mensajeError()
       }
     }
 
-    if (
-      (banderaNum === 1 && cantSymbols === 1) ||
-      (banderaNum === 1 && cantSymbols === 0)
-    ) {
+    if ((banderaNum === 1 && cantSymbols === 0) || (banderaNum === 1 && cantSymbols === 1)) {
       if (num2.length < 8) {
         num2 += e.target.textContent
         resultado.textContent += num2
+        console.log("simb-", cantSymbols);
+        console.log("bandera-", banderaNum);
+        return
       } else {
         resultado.textContent = num2
         mensajeError()
@@ -57,19 +58,20 @@ numeros.forEach(n => {
 // Agregando el evento a las operaciones
 operaciones.forEach(op => {
   op.addEventListener("click", e => {
-    if (
-      (cantSymbols === 0 && banderaNum === 0) ||
-      (cantSymbols === 1 && banderaNum === 0)
-    ) {
+    if ((banderaNum === 0 && cantSymbols === 0) || (banderaNum === 0 && cantSymbols === 1)) {
+      // console.log("Entró aquí");
       operacion = e.target.textContent
       banderaNum = 1
+      cantSymbols = 0
+      console.log("simb-", cantSymbols);
+      console.log("bandera-", banderaNum);
+      return
     }
-
-    if (
-      (cantSymbols === 0 && banderaNum === 1) ||
-      (cantSymbols === 1 && banderaNum === 1)
-    ) {
+    if ((banderaNum === 1 && cantSymbols === 0) || (banderaNum === 1 && cantSymbols === 1)) {
       operacion = e.target.textContent
+      console.log("simb-", cantSymbols);
+      console.log("bandera-", banderaNum);
+      return
     }
   })
 })
@@ -99,29 +101,37 @@ borrar.addEventListener("click", () => {
 
 // Evento del boton punto
 punto.addEventListener("click", e => {
-  if (cantSymbols === 0 && banderaNum === 0) {
-    if (num1.length < 8) {
-      if (num1 === "") {
-        num1 += "0"
+  if (cantSymbols === 0) {
+    if (banderaNum === 0) {
+      if (num1.length < 8) {
+        if (num1 === "") {
+          num1 += "0"
+        }
+        num1 += e.target.textContent
+        resultado.textContent = num1
+        cantSymbols = 1
+        console.log("simb-", cantSymbols);
+        console.log("bandera-", banderaNum);
+        return
+      } else {
+        mensajeError()
       }
-      num1 += e.target.textContent
-      resultado.textContent = num1
-      cantSymbols = 1
-    } else {
-      mensajeError()
     }
-  }
 
-  if (cantSymbols === 1 && banderaNum === 1) {
-    if (num2.length < 8) {
-      if (num2 === "") {
-        num2 += "0"
+    if (banderaNum === 1) {
+      if (num2.length < 8) {
+        if (num2 === "") {
+          num2 += "0"
+        }
+        num2 += e.target.textContent
+        resultado.textContent = num2
+        cantSymbols = 1
+        console.log("simb-", cantSymbols);
+        console.log("bandera-", banderaNum);
+        return
+      } else {
+        mensajeError()
       }
-      num2 += e.target.textContent
-      resultado.textContent = num2
-      cantSymbols = 0
-    } else {
-      mensajeError()
     }
   }
 })
@@ -137,7 +147,7 @@ reset.addEventListener("click", () => {
 
 // Evento del boton de igual
 igual.addEventListener("click", () => {
-  if (banderaNum === 1 && (cantSymbols === 0 || cantSymbols === 1)) {
+  if (banderaNum === 1) {
     let numeroRedondeado
     switch (operacion) {
       case "+":
@@ -150,25 +160,38 @@ igual.addEventListener("click", () => {
         numeroRedondeado = redondearNumero(parseFloat(num1) * parseFloat(num2))
         break
       case "/":
-        numeroRedondeado = redondearNumero(parseFloat(num1) / parseFloat(num2))
+        if (parseFloat(num2) !== 0) {
+          numeroRedondeado = redondearNumero(parseFloat(num1) / parseFloat(num2))
+        } else {
+          mensajeError("No se puede dividir por cero")
+        }
         break
       default:
         break
     }
     if (!isNaN(numeroRedondeado)) {
       resultado.textContent = numeroRedondeado
+      addHistorial(num1, num2, operacion, numeroRedondeado)
       num1 = `${numeroRedondeado}`
       num2 = ""
-      cantSymbols = 1
+      cantSymbols = 0
       banderaNum = 1
     }
+    console.log("simb-", cantSymbols);
+    console.log("bandera-", banderaNum);
   } else {
     mensajeError()
   }
 })
 
+resetBtn.addEventListener('click', () => {
+  calculos = []
+  limpiarHistorial()
+})
+
 // Funcion que lanza un error en pantalla
-function mensajeError() {
+function mensajeError(mensaje = "Error en estructura") {
+  error.textContent = mensaje
   error.classList.add("show")
   setTimeout(() => {
     error.classList.remove("show")
@@ -179,4 +202,26 @@ function mensajeError() {
 function redondearNumero(numero) {
   const factor = 10 ** 2
   return Math.round(numero * factor) / factor
+}
+
+// Funcion para adicionar la operacion al historial
+function addHistorial(num1, num2, op, result) {
+  const calculo = `${num1} ${op} ${num2} = ${result}`
+  calculos.push(calculo)
+  // Limpiar el historial para que no hayan repetidos
+  limpiarHistorial()
+  // Cargar todas las operaciones
+  calculos.forEach(operacion => {
+    const li = document.createElement("li")
+    li.classList.add("item-historial")
+    li.innerHTML = `${operacion}`
+    historial.appendChild(li)
+  })
+}
+
+// Funcion para limpiar el historial
+function limpiarHistorial() {
+  while (historial.firstChild) {
+    historial.removeChild(historial.firstChild)
+  }
 }
